2ac09bafd5372ff69e5854aefa824f8b43c3eee2
Charles Tuggle <tuggle.charles@gmail.com>
2019-11-25 18:32:58 -0500

    Updates

---
+++ precache-manifest.3e681d781667d53b02b1cd5c2f58234a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2c7ea374140aa48fed25daeeca3a35",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "bafdff661e3ae52286cf",
+    "url": "/clicky_game/static/css/main.9d6cd802.chunk.css"
+  },
+  {
+    "revision": "fbc5f9a76f588acbd67c",
+    "url": "/clicky_game/static/js/2.f28045fd.chunk.js"
+  },
+  {
+    "revision": "bafdff661e3ae52286cf",
+    "url": "/clicky_game/static/js/main.dae28533.chunk.js"
+  },
+  {
+    "revision": "3ebe35ebd05254742f82",
+    "url": "/clicky_game/static/js/runtime-main.d77cd24c.js"
+  }
+]);
